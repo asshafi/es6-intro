b3b759a46bc3e2b0597fbb8a4e0a9196204b2641
S M SHAFI <asshafi15@gmail.com>
2022-08-24 18:51:38 +0600

    arrow function practice

--- js/arrow.js
+++ js/arrow.js
@@ -1,19 +1,37 @@
 // function declaration:
+// function add(first, second) {
+//     const total = first + second;
+//     return total;
+// }
+
+// ----------------------------------------------
+// funciton expression:
+const add1 = function add1(first, second) {
+    const total = first + second;
+    return total;
+}
+// function expression with anonymous funciton 
+const add2 = function (first, second) {
+    const total = first + second;
+    return total;
+}
+// ----------------------------------------------
+// Arrow function:
 function add(first, second) {
     const total = first + second;
     return total;
 }
 
-
-// funciton expresion:
-const add1 = function add1(first, second) {
-    const total = first + second;
-    return total;
-}
-const add2 = function (first, second) {
-    const total = first + second;
-    return total;
+function add4(first, second) {
+    return first + second;
 }
 
+const add5=function (first, second) {
+    return first + second;
+}
+
+const add6 = (first, second) => first + second;
+
+// ==========================================
 const result = add(10, 20);
 console.log(result);
---
+++ js/more-arrow.js
@@ -0,0 +1,37 @@
+// Normal function:
+function add(a, b) {
+    return a + b;
+}
+
+// arrow function:
+
+// arrow function for 2 parameter:
+const add = (first, second) => first + second;
+const getFullName = (first, last) => first + ' ' + last;
+const multiply = (a, b) => a * b;
+
+const result = multiply(7, 8);
+// console.log(result);
+
+// arrow funciton  for more than 2 parameter :
+const addAll = (a, b, c, d, e, f) => a + b + c + d + e + f;
+
+
+// arrow function for no parameter:
+const getPie = () => 3.14;
+
+// one parameter:
+const doubleIt = (num) => num * 2;
+
+// one parameter simple version:
+const fiveTimes = num => num * 5;
+
+// multi line arrow function:
+// if you want to return something, use the return:
+const doMath = (x, y, z) => {
+    const firstSum = x + y;
+    const secondSum = y + z;
+    const multiplyResult = firstSum * secondSum;
+    const result = multiplyResult / 2;
+    return result;
+}
